6bba68354866a120f52e56163cee7dd8ab8dee1f
tammie07 <tamarawijnker@gmail.com>
2021-12-02 11:28:34 +0100

    seperated pages

--- flutter/lib/fragments/vote_fragment.dart
+++ flutter/lib/fragments/vote_fragment.dart
@@ -3,7 +3,7 @@ import 'package:am_awareness/fragments/home_fragment.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:am_awareness/components/photo.dart';
-
+import 'package:am_awareness/fragments/vote_detail_fragment.dart';
 
 class VoteFragment extends StatefulWidget {
   const VoteFragment({Key? key, required this.uuid}) : super(key: key);
@@ -16,116 +16,84 @@ class VoteFragment extends StatefulWidget {
 
 class _VoteFragmentState extends State<VoteFragment> {
   final List<Photo> _photos = [
-    Photo(
-        'images/auto.jpg',
-        'Auto',
-        'Een simpele auto, c1 rood',
-        'rood, verbrandingsmotor, auto',
-        'Arnhem'
-    ),
+    Photo('images/auto.jpg', 'Auto', 'Een simpele auto, c1 rood',
+        'rood, verbrandingsmotor, auto', 'Arnhem'),
     Photo(
         'images/bierfiets.jpg',
         'Bierfiets',
         'Een fiets voor vrienden, terwijl je bier drinkt',
         'Fiets, bier, alcohol',
-        'Nijmegen'
-    ),
-    Photo(
-        'images/e-step.jpg',
-        'e-step',
-        'Een step met een electrische motor',
-        'step, electrisch, klein',
-        'Wychen'
-    )
+        'Nijmegen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/e-step.jpg', 'e-step', 'Een step met een electrische motor',
+        'step, electrisch, klein', 'Wychen'),
+    Photo('images/auto.jpg', 'Auto', 'Een simpele auto, c1 rood',
+        'rood, verbrandingsmotor, auto', 'Arnhem'),
+    Photo('images/auto.jpg', 'Auto', 'Een simpele auto, c1 rood',
+        'rood, verbrandingsmotor, auto', 'Arnhem'),
+    Photo('images/auto.jpg', 'Auto', 'Een simpele auto, c1 rood',
+        'rood, verbrandingsmotor, auto', 'Arnhem'),
+    Photo('images/auto.jpg', 'Auto', 'Een simpele auto, c1 rood',
+        'rood, verbrandingsmotor, auto', 'Arnhem'),
   ];
 
   @override
   void initState() {
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Test Event')),
-        body: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisSpacing: 0,
-              mainAxisSpacing: 0,
-              crossAxisCount: 4,
-            ),
-            itemCount: _photos.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: (){
-                  Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => DetailPage(
-                        image: _photos[index].image,
-                        title: _photos[index].title,
-                        description: _photos[index].description,
-                        tags: _photos[index].tags,
-                        location: _photos[index].location
-                      ),
-                  ),
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(_photos[index].image),
-                    ),
-                  ),
+      appBar: AppBar(title: const Text('Test Event')),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 0,
+          crossAxisCount: 4,
+        ),
+        itemCount: _photos.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailPage(
+                      image: _photos[index].image,
+                      title: _photos[index].title,
+                      description: _photos[index].description,
+                      tags: _photos[index].tags,
+                      location: _photos[index].location),
                 ),
               );
             },
-        ),
-    );
-  }
-}
-class DetailPage extends StatelessWidget {
-  final String image;
-  final String title;
-  final String description;
-  final String tags;
-  final String location;
-  const DetailPage({Key? key,
-    required this.image, required this.title, required this.description, required this.location, required this.tags })
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        children: [
-          AspectRatio(
-              aspectRatio: 1,
-          child: Container(
-            width: double.infinity,
-            child: Image(
-              image: AssetImage(image),
-            ),
-          ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20.0),
-            child: Center(
-              child: Text(
-                title,
-                style: TextStyle(fontSize: 40),
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(_photos[index].image),
+                ),
               ),
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
 }
-
