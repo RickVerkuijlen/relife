5bc69139de0e7d56b16c00c56dca11a99608cfe3
tammie07 <tamarawijnker@gmail.com>
2021-12-15 17:09:40 +0100

    Made offers visible

--- flutter/lib/fragments/shop_fragment.dart
+++ flutter/lib/fragments/shop_fragment.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:am_awareness/components/voucher.dart';
 
 List<Voucher> voucher = [
-  Voucher('images/bier.jpg', 'Gratis Biertje', '500', 'images/lokaal_99.jpg')
+  Voucher('images/bier.jpg', 'Gratis Biertje', '500', 'images/lokaal_99.jpg'),
+  Voucher('images/bier.jpg', 'Gratis Biertje', '500', 'images/lokaal_99.jpg'),
 ];
 
 class ShopFragment extends StatefulWidget {
-  const ShopFragment({Key? key}) : super (key: key);
+  const ShopFragment({Key? key}) : super(key: key);
   @override
   _AddFragment createState() => _AddFragment();
 }
@@ -16,9 +17,65 @@ class _AddFragment extends State<ShopFragment> {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        Center(
-          child: Text("Welcome to the shop page"),
+      children: [
+        Container(
+          padding: EdgeInsets.all(8.0),
+          height: MediaQuery.of(context).size.height / 2,
+          width: MediaQuery.of(context).size.width,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 2,
+              mainAxisSpacing: 2,
+              crossAxisCount: 2,
+            ),
+            itemCount: voucher.length,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10.0,
+                        spreadRadius: 2.0,
+                        offset: Offset(0.0, 0.0),
+                      ),
+                    ]),
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height / 7,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20)),
+                          image: DecorationImage(
+                              image: AssetImage(voucher[index].image),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height / 11,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage(voucher[index].logo),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
         )
       ],
     );
